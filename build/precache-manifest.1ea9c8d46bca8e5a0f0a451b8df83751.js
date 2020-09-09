1217ac5867528d5bbca6e92cc5c7c54f3626b06b
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-09-09 16:11:27 -0500

    build

---
+++ build/precache-manifest.1ea9c8d46bca8e5a0f0a451b8df83751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009d88f7562a9ec15a86f8a6f2dbec0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c912bf933a24add118",
+    "url": "/static/css/main.7c51f1f5.chunk.css"
+  },
+  {
+    "revision": "853ffbe0cb8ea92d5cca",
+    "url": "/static/js/2.896e2e93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.896e2e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c912bf933a24add118",
+    "url": "/static/js/main.30a11693.chunk.js"
+  },
+  {
+    "revision": "f2c6337e377efda0ca81",
+    "url": "/static/js/runtime-main.388da5f5.js"
+  }
+]);
